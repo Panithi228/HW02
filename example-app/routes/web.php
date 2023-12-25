64b1a73b245a05bed2bf302a536aed0022c20774
Panithi228 <65160228@go.buu.ac.th>
2023-12-25 18:37:27 +0700

    update

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-route', function() {
+    echo "<h1>My Route Page</h1>";
+});
