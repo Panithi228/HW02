5ac1b60fdea0d09acc335943ba20ad259566c2ec
Panithi228 <65160228@go.buu.ac.th>
2023-12-25 17:22:42 +0700

    Update index_workshop.php

--- index_workshop.php
+++ index_workshop.php
@@ -1,32 +1,30 @@
 <html>
-    <body>
-        <h1>This is PHP <?php echo 'Hello World!'; ?></h1>
-        <div>
-            <p>1</p>
-            <?php
-            $Var_a = 1;
-            $Var_a = "1$Var_a";
-            print_r($Var_a);
-            echo"<br>";
-            #var_dump($Var_a);
-            echo $Var_a + 2;
-            $arry = array();
-            $arry[] = [];
-            $arry[] = 1;
-            $arry[] = 2;
-            $arry[0] = 3;
-            print_r($arry);
-            // count
-            // sizeof
-            for($i = 0; $i < count($arry); $i++)
-            {
-                echo "<h1>my h1</h1>";
-                ?>
-                <p>2</p>
-                <?php 
-            }
-            ?>
-            <p>3</p>
-        </div>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
 </html>
