7271286af0a63f9c02bb1c44e0853032e09f34ce
Panithi228 <65160228@go.buu.ac.th>
2023-12-25 17:42:34 +0700

    edit

--- index.php
+++ index.php
@@ -5,68 +5,23 @@
 </head>
 
 <body>
-    <h1><?php echo "Hello World<br><br>"; ?></h1>
-    <?php echo "<h1>Hello World<br><br><h1>" ?>
-    <?php $var_a = "SE CAMP" ?>
-    <?php echo $var_b = 'Hello $var_a' ?>
-    <br>
-    <?php echo $var_c = "Hello $var_a" ?>
-    <br>
-    <?php
-    $var_c = $var_b = 1;
-    $var_c = '1';
-    echo $var_b <=> $var_c;
-    echo "<br>";
-    if($var_b === $var_c){
-        // true
-        echo '$var_b === $var_c';
-    }else if($var_b <> $var_c){
-        // true
-        echo '$var_b <> $var_c';
-    }else{
-        // false
-        echo 'else';
-    }?>
-    <br>
-    <?php echo $var_C = 2; ?>
-    <br>
-    <?php 
-    echo $var_c; 
-    echo"<br>";
-    switch($var_c){
-        case 1:
-            echo "1";
-            break;
-        default:
-            echo "test";
-    }
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <div>
+        <?php for($i = 1; $i <= 24; $i++) { ?>
+            <?php echo $multi_x ?> x <?php echo $i ?> = <?php echo $multi_x*$i ?>
+            <br>
+        <?php } ?>       
+    </div>
     
-    $arry = array(1,2,3);
-    $arry1 = [1,2,3]; //{}
-    $arry2[] = 1;
-    $arry2[] = 2;
-    $arry2[] = 3;
-    $arry2[] = "Hello";
-    $arry2['SE'] = "World";
-    ?>
-    <pre><?php print_r($arry);?></pre>
-    <pre><?php print_r($arry1);?></pre>
-    <pre><?php print_r($arry2);?></pre>
-    <?php for($i = 0; $i < count($arry2); $i++){ ?>
-    <h3><?php echo $arry2[$i]; ?></h3>
-    <?php } ?>
-    <?php foreach($arry2 as $key => $value){ ?>
-    <h3><?php echo $arry2[$key]; ?> : <?php echo $value; ?></h3>
-    <?php } ?>
-    <?php 
-        pre($arry);
-    ?>
-    <?php
-    function pre($value){
-        echo "<pre>1";
-        print_r($value);
-        echo "</pre>";
-    } ?>
+    <style>
+        div{
+            width: 200;
+            height: 350;
+            border: 50px;
+            overflow: scroll;
+        }
+    </style>
 </body>
 
 </html>
