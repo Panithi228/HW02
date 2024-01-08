82e65d3532a8b68ac5dc456a1bd64470da0ae5d9
Panithi228 <65160228@go.buu.ac.th>
2024-01-08 21:54:36 +0700

    update page and route

--- example-app/resources/views/myfolder/mypage.blade.php
+++ example-app/resources/views/myfolder/mypage.blade.php
@@ -1,12 +1,26 @@
-<h1>My Folder and My Page</h1>
-<!-- <h1>
-    <?php echo $val_a . ' ' . $val_b; ?>
-</h1> -->
-<!-- <h1>{{$val_a}} {{$val_b}}</h1> -->
-<!-- <h1>{{$val_a . ' ' . $val_b}}</h1> -->
-<form method="post" action="/my-route">
-    input number
-    @csrf
-    <input type="text" name="myinput">
-    <button type="submit">Submit</button>
-</form>
+<body>
+    <center>
+         <h1>My Folder and My Page</h1>
+        <!-- <h1>
+            <?php echo $val_a . ' ' . $val_b; ?>
+        </h1> -->
+        <!-- <h1>{{$val_a}} {{$val_b}}</h1> -->
+        <!-- <h1>{{$val_a . ' ' . $val_b}}</h1> -->
+        <form method="post" action="/my-route">
+            <p style="color: white">input number</p>
+            @csrf
+            <input type="text" name="myinput">
+            <button type="submit">Submit</button>
+        </form>
+    </center>
+</body>
+
+<style>
+    body{
+        background-color: black;
+    }
+
+    h1{
+        color: rgb(23, 221, 145);
+    }
+</style>
--- example-app/resources/views/myroute.blade.php
+++ example-app/resources/views/myroute.blade.php
@@ -2,13 +2,13 @@
     <head>
         <meta charset="utf-8" />
     </head>
-    <body>
+    <body style="background-color:black">
         <!-- <h1>My Route Page</h1> -->
         <!-- <h1>{{ $myinput }}</h1> -->
         <center>
             <?php $multi_x = 2; ?>
-        <h1 style="color:blue">ตารางสูตรคูณแม่ <?php echo $myinput ?></h1>
-        <div>
+        <h1>ตารางสูตรคูณแม่ <?php echo $myinput ?></h1>
+        <div class="tableFont">
             <?php for($i = 1; $i <= 24; $i++) { ?>
                 <?php echo $myinput ?> x <?php echo $i ?> = <?php echo $myinput*$i ?>
                 <br>
@@ -22,7 +22,16 @@
                 height: 350;
                 border: 1px;
                 text-align: left;
-                overflow: scroll;
+                overflow-y: scroll;
+                color: white;
+            }
+            h1{
+                background-image: linear-gradient(90deg, rgb(0, 255, 98), rgb(60, 255, 0));
+                background-clip: text;
+                -webkit-text-stroke-color: transparent;
+                -webkit-text-stroke-width: calc(1em / 16);
+                -webkit-background-clip: text;
+                font-size: 45px;
             }
         </style>
     </body>
