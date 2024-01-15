bd430fe75ac8132a3c99873535e108808c09b82c
Panithi228 <65160228@go.buu.ac.th>
2024-01-15 14:03:34 +0700

    Update web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/my-controller", [MyController::class, "index"]);
+Route::get("/my-controller2", 'App\Http\Controllers\MyController@index');
 Route::get('/', function () {
     return view('welcome');
 });
