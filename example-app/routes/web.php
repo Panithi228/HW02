122f3cac610255f5d43c01ec0ff010b3859a7816
Panithi228 <65160228@go.buu.ac.th>
2024-01-15 13:47:24 +0700

    add controller

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/my-controller", [MyController::class, "index"]);
 Route::get('/', function () {
     return view('welcome');
 });
