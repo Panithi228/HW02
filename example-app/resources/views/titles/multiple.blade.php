c266d5d5d6591edc18ea239e91e9c9ce0f8c432f
Panithi228 <65160228@go.buu.ac.th>
2024-02-15 12:36:05 +0700

    multiplication

--- example-app/resources/views/titles/multiple.blade.php
+++ example-app/resources/views/titles/multiple.blade.php
@@ -8,30 +8,28 @@
         <body>
             <br>
             <center>
-                <input type="number" id="mul_number">
-                <button onclick="multiple()"> Calculate !</button>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+                <div>
+                    <input type="number" id="mul_number" value="2" class="text text-center">
+                    <button onclick="multiple()"> Calculate !</button>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+                </div>
                 <script>
+                    $(document).ready(function(){
+                         console.log("ready");
+                    });
                     function multiple(){
-                        let multiple_num = parseInt($('#mul_number').val());
-                        for(let i = 1; i < 25; i++){
-                            // console.log(multiple_num * i);
-                            $('#myh1').before(`<center><h1 class="gen_number">${multiple_num} x ${i} = ${multiple_num * i}</h1></center>`)
+                        $('.my_gen_number').remove();
+                        let mul_number = parseInt($('#mul_number').val());
+                        $('#number').after(`<h1 class="my_gen_number , myh1"> ${"สูตรคูณแม่ : " + mul_number} </h1>`);
+                        for (let i = 1; i <= 24; i++) {
+                            $('#myh1').before(`<h4 class="my_gen_number ,myh1"> ${mul_number + " x " + i + " = " + (i) * mul_number }</h4>`);
                         }
-                        // setTimeout(function(){
-                        // $(document).getElementById("myh1").remove();
-                        // }, 100);
+                        console.log(document.getElementById("เลขที่กรอก : " + 'mul_number').value);
                     }
-
                 </script>
+                <br>
+                <h1 id="myh1"></h1>
             </center>
-            <br>
-            <h1 id="myh1"></h1>
-            <style>
-                h1{
-                    font-size: 20px;
-                }
-            </style>
         </body>
     </html>
 @endsection
